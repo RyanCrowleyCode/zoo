4d91cce827a6833fb242bb8dc3cb1f4da0beb307
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-30 10:03:48 -0600

    chapter code for PaintedDog complete

--- zoo.py
+++ zoo.py
@@ -1,15 +1,19 @@
-from animals import Penguin
+from animals import Penguin, PaintedDog
 from habitats import Habitat
 
 # Create a penguin
 bob = Penguin("Bob")
-print(bob)
-bob.run()
-bob.swim()
+# print(bob)
+# bob.run()
+# bob.swim()
+
+# Create a painted dog
+ralph = PaintedDog("Ralph")
 
 # Create a habitat
 seaworld = Habitat("Sea World")
 seaworld.add_animal(bob)
+seaworld.add_animal(ralph)
 
 for animal in seaworld.animals:
     print(animal)
