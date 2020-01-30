15c76134d4227b637592a0479bd01580ece36668
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-30 10:01:04 -0600

    code completed for Habitat class

--- zoo.py
+++ zoo.py
@@ -1,6 +1,15 @@
 from animals import Penguin
+from habitats import Habitat
 
+# Create a penguin
 bob = Penguin("Bob")
 print(bob)
 bob.run()
 bob.swim()
+
+# Create a habitat
+seaworld = Habitat("Sea World")
+seaworld.add_animal(bob)
+
+for animal in seaworld.animals:
+    print(animal)
